208495bb6f2fc61df99c41314f4f41461ac5f3a4
Rabeeh T A <rabeehchemnad@gmail.com>
2025-08-25 15:25:53 +0530

    remove vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import Products from "@/components/ui/products";
 import Footer from "@/components/ui/footer";
 import SiteNavbar from "@/components/ui/site-navbar";
 import { Connect } from "@/components/ui/connect";
-import { Analytics } from '@vercel/analytics/next';
-
 
 export default function Home() {
   return (
     <div>
-      <Analytics />
       <SiteNavbar />
       <InteractiveHero />
       <Features />
